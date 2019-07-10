843e8d82d1688a24bce9f9b6895a297ecd5152bb
eldaPro <faith030@gmail.com>
2019-07-10 16:42:02 +0700

    connect component - Navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,12 +1,16 @@
 // ---------------------------------------------- modules import
 import React, { FunctionComponent } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+
+import { INavbarProps } from "./types";
+import { AppState } from "../../store/rootReducer";
 
 import LoggedInLink from "./loggedInLink";
 import LoggedOutLink from "./loggedOutLink";
 
 // ---------------------------------------------- the component
-const Navbar: FunctionComponent = () => (
+const Navbar: FunctionComponent<INavbarProps> = ({ token }) => (
   <nav className="nav-wrapper yellow darken-2" style={{ marginBottom: "50px" }}>
     <div className="container">
       <Link to="/">
@@ -17,12 +21,14 @@ const Navbar: FunctionComponent = () => (
           React Ts Demo
         </span>
       </Link>
-      <ul className="right">
-        <LoggedInLink />
-        <LoggedOutLink />
-      </ul>
+      <ul className="right">{token ? <LoggedInLink /> : <LoggedOutLink />}</ul>
     </div>
   </nav>
 );
 
-export default Navbar;
+// ---------------------------------------------- map state to props
+const mapStateToProps = (state: AppState) => ({
+  token: state.auth.token
+});
+
+export default connect(mapStateToProps)(Navbar);
