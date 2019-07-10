2e88c270dc577d2fae1b66202059c01be604d028
eldaPro <faith030@gmail.com>
2019-07-10 16:10:13 +0700

    create component - AddProduct

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@
 import React, { FunctionComponent } from "react";
 
 // import Login from "./components/auth/login";
-import Register from "./components/auth/register";
+// import Register from "./components/auth/register";
 // import Dashboard from "./components/layout/dashboard";
 import Navbar from "./components/layout/navbar";
+import AddProduct from "./components/product/addProduct";
 
 // ---------------------------------------------- the component
 const App: FunctionComponent = () => (
   <div className="App">
     <Navbar />
-    <Register />
+    <AddProduct />
   </div>
 );
 
---
+++ src/components/product/addProduct.tsx
@@ -0,0 +1,96 @@
+// ---------------------------------------------- modules import
+import M from "materialize-css";
+import React, {
+  ChangeEvent,
+  FormEvent,
+  FunctionComponent,
+  useEffect,
+  useState
+} from "react";
+
+// ---------------------------------------------- the component
+const AddProduct: FunctionComponent = () => {
+  // ---------------------------------------------- local state
+  const [product, setProduct] = useState({
+    id: 0,
+    name: "",
+    price: 0,
+    unitCost: 0
+  });
+
+  // ---------------------------------------------- handlers
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.id === "name") {
+      setProduct({ ...product, [e.currentTarget.id]: e.currentTarget.value });
+    } else {
+      setProduct({
+        ...product,
+        [e.currentTarget.id]: parseInt(e.currentTarget.value)
+      });
+    }
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log("add product");
+    console.log(product);
+  };
+
+  // ---------------------------------------------- effects
+  useEffect(() => {
+    M.AutoInit();
+  }, []);
+
+  // ---------------------------------------------- content
+  return (
+    <div className="container">
+      <h5 className="center-align">ADD PRODUCT</h5>
+      <div className="row">
+        <form className="col s12 m6 offset-m3" onSubmit={handleSubmit}>
+          <div className="input-field">
+            <input type="text" id="name" onChange={handleChange} required />
+            <label htmlFor="name">Name</label>
+          </div>
+          <div className="input-field">
+            <input
+              type="number"
+              id="price"
+              min={0}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="price">Price</label>
+          </div>
+          <div className="input-field">
+            <input
+              type="number"
+              id="unitCost"
+              min={0}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="unitCost">Unit Cost</label>
+          </div>
+          <div className="input-field center">
+            <button
+              type="submit"
+              className={`btn grey darken-4 ${
+                product.name.length && product.price && product.unitCost
+                  ? ""
+                  : "disabled"
+              }`}
+            >
+              Add Product
+            </button>
+          </div>
+          <div className="input-field red-text center">
+            <p>Error Add Product here.</p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddProduct;
