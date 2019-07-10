eff249f513e529ee23369a56cb82682a005ff510
eldaPro <faith030@gmail.com>
2019-07-10 16:06:14 +0700

    create component - Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 // ---------------------------------------------- modules import
 import React, { FunctionComponent } from "react";
 
-import Dashboard from "./components/layout/dashboard";
+import Login from "./components/auth/login";
+// import Dashboard from "./components/layout/dashboard";
 import Navbar from "./components/layout/navbar";
 
 // ---------------------------------------------- the component
 const App: FunctionComponent = () => (
   <div className="App">
     <Navbar />
-    <Dashboard />
+    <Login />
   </div>
 );
 
